28c9cff18f995587f22180d9094ba368e581c459
Kevin Lewis <kevin@lewistech.com>
2012-09-28 19:15:51 -0700

    initial commit

---
+++ LedTest1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace LedTest1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            //const int firstpin = (int) Cpu.Pin.GPIO_Pin8;
+            //const int pinCount = 3;
+
+            var currentPin = 0;
+
+            var leds = new[] {new OutputPort(Pins.GPIO_PIN_D8, false), new OutputPort(Pins.GPIO_PIN_D9, false), new OutputPort(Pins.GPIO_PIN_D10, false)};
+
+            var button = new InterruptPort(Pins.ONBOARD_SW1, false, Port.ResistorMode.Disabled, Port.InterruptMode.InterruptEdgeBoth);
+
+            button.OnInterrupt += (data1, data2, time) =>
+                {
+                    leds[currentPin%3].Write(data2 == 1);
+
+                    if (currentPin >= 3)
+                        leds[(currentPin + 1)%3].Write(data2 == 1);
+
+                    if (currentPin >= 6)
+                        leds[(currentPin + 2)%3].Write(data2 == 1);
+
+                    if (data2 == 0)
+                    {
+                        currentPin++;
+                        if (currentPin > 6)
+                            currentPin = 0;
+                    }
+                };
+
+
+            Thread.Sleep(Timeout.Infinite);
+
+        }
+
+    }
+}
---
+++ PomodoroTimer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace PomodoroTimer
+{
+    public class Program
+    {
+        private static OutputPort led;
+        private static InterruptPort button;
+
+        private static Timer timer;
+        private static Timer timerFinishedFlash;
+
+        public static void Main()
+        {
+            // write your code here
+            led = new OutputPort(Pins.ONBOARD_LED, false);
+
+            button = new InterruptPort(Pins.ONBOARD_SW1, false, Port.ResistorMode.Disabled,Port.InterruptMode.InterruptEdgeLow);
+            button.OnInterrupt += ToggleTimer;
+
+            Thread.Sleep(Timeout.Infinite);
+
+
+        }
+
+        private static void ToggleTimer(uint data1, uint data2, DateTime time)
+        {
+
+            if (timerFinishedFlash != null)
+            {
+                timerFinishedFlash.Dispose();
+                timerFinishedFlash = null;
+            }
+
+            if (timer == null)
+            {
+                StartTimer();
+            }
+            else
+            {
+                timer.Dispose();
+                timer = null;
+            }
+        }
+
+        private static void StartTimer()
+        {
+            timer = new Timer(TimerFinished, null, new TimeSpan(0, 25, 0), TimeSpan.FromTicks(-TimeSpan.TicksPerMillisecond));
+            FlashOnOff();
+            Thread.Sleep(100);
+            FlashOnOff();
+
+        }
+
+        private static void TimerFinished(object state)
+        {
+            timer.Dispose();
+            timer = null;
+
+            timerFinishedFlash = new Timer(FlashOnOff, null, 0,200);
+            
+        }
+
+        private static void FlashOnOff(object state = null)
+        {
+            led.Write(true);
+            Thread.Sleep(100);
+            led.Write(false);
+        }
+    }
+
+    
+}
